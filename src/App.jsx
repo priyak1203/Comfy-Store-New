00fc4f544548c6c9250ce97dff05d64468055630
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-04 23:47:25 +0530

    app with router and pages basic structure

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  About,
+  Cart,
+  Checkout,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
