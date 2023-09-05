7f2668e5ec8de286cf5c417808240b4f6b5ba72c
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-05 13:04:52 +0530

    login page with styles

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -10,7 +10,7 @@ const Error = () => {
         <div className="text-center">
           <p className="text-9xl font-semibold text-primary">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-            page not found
+            Page not found
           </h1>
           <p className="mt-6 text-lg leading-7">
             Sorry, we couldn'g find the page you are looking for.
