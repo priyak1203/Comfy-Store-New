680d5c7738c0247fc81769d59004566a541caa9c
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-06 23:11:25 +0530

    featured products

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,5 +1,13 @@
+import ProductsGrid from './ProductsGrid';
+import SectionTitle from './SectionTitle';
+
 const FeaturedProducts = () => {
-  return <div>FeaturedProducts</div>;
+  return (
+    <div className="pt-24">
+      <SectionTitle text="featured products" />
+      <ProductsGrid />
+    </div>
+  );
 };
 
 export default FeaturedProducts;
--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,5 @@ export { default as FormInput } from './FormInput';
 export { default as Header } from './Header';
 export { default as Hero } from './Hero';
 export { default as Navbar } from './Navbar';
+export { default as SectionTitle } from './SectionTitle';
 export { default as SubmitBtn } from './SubmitBtn';
