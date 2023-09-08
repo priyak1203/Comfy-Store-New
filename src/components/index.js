9481c6c977f4a8fd90237f1997557efa84c640b5
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-08 15:17:31 +0530

    global loading in home layout

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { default as Filters } from './Filters';
 export { default as FormInput } from './FormInput';
 export { default as Header } from './Header';
 export { default as Hero } from './Hero';
+export { default as Loading } from './Loading';
 export { default as Navbar } from './Navbar';
 export { default as PaginationContainer } from './PaginationContainer';
 export { default as ProductsContainer } from './ProductsContainer';
