75ada2cfac334cb87df5ce3fde8daf6ee930da3e
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-08 12:58:57 +0530

    range input for price in filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,6 +1,7 @@
 import { Form, Link, useLoaderData } from 'react-router-dom';
 import FormInput from './FormInput';
 import FormSelect from './FormSelect';
+import FormRange from './FormRange';
 
 const Filters = () => {
   const { meta } = useLoaderData();
@@ -36,6 +37,8 @@ const Filters = () => {
         list={['a-z', 'z-a', 'high', 'low']}
         size="select-sm"
       />
+      {/* PRICE */}
+      <FormRange name="price" label="select price" size="range-sm" />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
         search
