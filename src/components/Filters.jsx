97baf2c49dad579a349152bddb13d050f1ad7a44
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-08 14:57:56 +0530

    checkbox input in the filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,6 +2,7 @@ import { Form, Link, useLoaderData } from 'react-router-dom';
 import FormInput from './FormInput';
 import FormSelect from './FormSelect';
 import FormRange from './FormRange';
+import FormCheckbox from './FormCheckbox';
 
 const Filters = () => {
   const { meta } = useLoaderData();
@@ -39,11 +40,13 @@ const Filters = () => {
       />
       {/* PRICE */}
       <FormRange name="price" label="select price" size="range-sm" />
+      {/* SHIPPING */}
+      <FormCheckbox name="shipping" label="free shipping" size="checkbox-sm" />
       {/* BUTTONS */}
-      <button type="submit" className="btn btn-primary btn-sm">
+      <button type="submit" className="btn btn-primary btn-sm mt-4">
         search
       </button>
-      <Link to="/products" className="btn btn-accent btn-sm">
+      <Link to="/products" className="btn btn-accent btn-sm mt-4">
         reset
       </Link>
     </Form>
