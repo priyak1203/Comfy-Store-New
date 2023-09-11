174606a037abfe5a347c82a14dedab0186bddf50
Priydarshini Karanth <kpriya1203@gmail.com>
2023-09-11 13:29:00 +0530

    register user functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,32 @@
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, redirect } from 'react-router-dom';
 import { FormInput, SubmitBtn } from '../components';
+import { customFetch } from '../utils/helpers';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    const response = await customFetch.post(`/auth/local/register`, data);
+    toast.success('Account created successfully');
+    return redirect('/login');
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      'please double check your credentials';
+    toast.error(errorMessage);
+    return null;
+  }
+};
 
 const Register = () => {
   return (
     <section className="h-screen grid place-items-center">
-      <Form className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
+      <Form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
         <h4 className="text-center text-3xl font-bold">Register</h4>
         <FormInput type="text" label="username" name="username" />
         <FormInput type="email" label="email" name="email" />
